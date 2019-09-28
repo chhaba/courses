173e2ddc533623f1436aa7f642b92e7eba8a2610
chhaba <chhaba@gmail.com>
2019-09-28 13:33:14 +0530

    added JPA sample code

---
+++ demo/src/main/java/com/codotics/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.codotics.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Application.");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Exiting Application.");
+	}
+
+}
