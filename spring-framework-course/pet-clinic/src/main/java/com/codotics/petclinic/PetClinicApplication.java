584aa77fb1d42d046cb08af20c6fda7a21723ae9
chhaba <chhaba@gmail.com>
2019-09-29 11:14:35 +0530

    Updated .gitignore

--- spring-framework-course/pet-clinic/src/main/java/com/codotics/petclinic/PetClinicApplication.java
+++ spring-framework-course/pet-clinic/src/main/java/com/codotics/petclinic/PetClinicApplication.java
@@ -3,6 +3,11 @@ package com.codotics.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Codotics Technologies
+ *
+ */
 @SpringBootApplication
 public class PetClinicApplication {
 
--- spring-framework-course/pet-clinic/src/test/java/com/codotics/petclinic/PetClinicApplicationTests.java
+++ spring-framework-course/pet-clinic/src/test/java/com/codotics/petclinic/PetClinicApplicationTests.java
@@ -3,6 +3,11 @@ package com.codotics.petclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * 
+ * @author Codotics Technologies
+ *
+ */
 @SpringBootTest
 class PetClinicApplicationTests {
 
