f3ff68ff9ed54a3db24b55cbcfe4632427f40132
chhaba <chhaba@gmail.com>
2019-10-07 17:23:05 +0530

    Spring Boot Profile Properties Files

--- spring-framework-course/di-demo/src/main/java/com/codotics/DiDemoApplication.java
+++ spring-framework-course/di-demo/src/main/java/com/codotics/DiDemoApplication.java
@@ -2,7 +2,6 @@ package com.codotics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.system.ApplicationPid;
 import org.springframework.context.ApplicationContext;
 
 import com.codotics.controllers.MyController;
@@ -17,6 +16,8 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) ctx.getBean("myController");
 
+		System.out.println(controller.hello());
+		
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUser());
 		System.out.println(fakeDataSource.getPassword());
