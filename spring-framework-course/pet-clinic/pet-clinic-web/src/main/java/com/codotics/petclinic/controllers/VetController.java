28f0a67c68db00ce215539aad59126a539860d8a
chhaba <chhaba@gmail.com>
2019-10-07 10:13:43 +0530

    List all Vets on Vet index page. Closes #14

--- spring-framework-course/pet-clinic/pet-clinic-web/src/main/java/com/codotics/petclinic/controllers/VetController.java
+++ spring-framework-course/pet-clinic/pet-clinic-web/src/main/java/com/codotics/petclinic/controllers/VetController.java
@@ -4,7 +4,10 @@
 package com.codotics.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codotics.petclinic.services.VetService;
 
 /**
  * @author Codotics Technologies
@@ -13,8 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
